339d32b744702b3719beeb9d966312152b9c820b
Christian Rey Piape <harenzp14@gmail.com>
2023-05-25 15:23:02 +0800

    toast

--- app/src/main/java/com/project/bikersden/Services.java
+++ app/src/main/java/com/project/bikersden/Services.java
@@ -2,8 +2,11 @@ package com.project.bikersden;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Services extends AppCompatActivity {
 
@@ -16,5 +19,13 @@ public class Services extends AppCompatActivity {
         getSupportActionBar().hide();
 
         proceed = (Button) findViewById(R.id.proceed);
+
+        proceed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Services.this, "Schedule set. Just present your account details at the shop tomorrow.", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(Services.this, WelcomeScreen.class));
+            }
+        });
     }
 }
